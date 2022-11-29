8b6606df7880c6964e85465a589fd38ebbc605a9
lubuntum <lubuntum@gmail.com>
2022-11-29 09:51:22 +0700

    Добавил фильтрацию при отправке задачи + реализовал полностью выбор и проверку дат, отображение

--- app/src/main/java/com/pavel/databaseapp/taskfragment/CreateTaskFragment.java
+++ app/src/main/java/com/pavel/databaseapp/taskfragment/CreateTaskFragment.java
@@ -1,22 +1,35 @@
 package com.pavel.databaseapp.taskfragment;
 
+import android.app.DatePickerDialog;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
+import com.pavel.databaseapp.R;
 import com.pavel.databaseapp.data.Employee;
+import com.pavel.databaseapp.data.Task;
 import com.pavel.databaseapp.databinding.FragmentCreateTaskBinding;
 import com.pavel.databaseapp.dialog.EmployeeSearchDialog;
 import com.pavel.databaseapp.dialog.EmployeeViewModel;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class CreateTaskFragment extends Fragment {
     FragmentCreateTaskBinding binding;
@@ -48,20 +61,126 @@ public class CreateTaskFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        binding.searchEmployee.setOnClickListener((v) ->{
-            EmployeeSearchDialog searchDialog = new EmployeeSearchDialog();
-            searchDialog.show(getParentFragmentManager(),"search_fragment");
-        });
-        Observer<Employee> pickedEmployeeObserver = new Observer<Employee>() {
+        inputEmployeeMonitoringInit();//Для отслеживания ввода имени работника
+        searchEmployeeInit();//Поиск работников, открытие окна при нажатии на лупу и тд
+        datePickerInit();//Инициализация выбора даты
+        sendDataFilter();//фильтрация отправляемых данных, проверка
+        initFilterStatus();//для отображения статуса отправки
+   }
+   private void datePickerInit(){
+       //Читай события низу вверх
+       Calendar actualCalendar = Calendar.getInstance();
+       //Запоминают выбранные даты для их сравнения
+       Calendar startDateCalendar = Calendar.getInstance();
+       Calendar endDateCalendar = Calendar.getInstance();
+       SimpleDateFormat dateFormat = new SimpleDateFormat("dd.mm.yyyy");
+       DatePickerDialog.OnDateSetListener endDatePickListener = new DatePickerDialog.OnDateSetListener() {
+           @Override
+           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+               endDateCalendar.set(year,month,dayOfMonth);
+               if (endDateCalendar.before(startDateCalendar))
+                   Toast.makeText(getContext(), "Не корректно выбраны даты", Toast.LENGTH_SHORT).show();
+               else {
+                   String startDateStr = dateFormat.format(startDateCalendar.getTime());
+                   String endDateStr = dateFormat.format(endDateCalendar.getTime());
+                   binding.startDate.setText(startDateStr);
+                   binding.endDate.setText(endDateStr);
+               }
+           }
+       };
+       DatePickerDialog.OnDateSetListener startDatePickListener = new DatePickerDialog.OnDateSetListener() {
+           @Override
+           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+               startDateCalendar.set(year,month,dayOfMonth);
+               DatePickerDialog endDatePicker = new DatePickerDialog(
+                       getContext(), endDatePickListener,
+                       actualCalendar.get(Calendar.YEAR),
+                       actualCalendar.get(Calendar.MONTH),
+                       actualCalendar.get(Calendar.DAY_OF_MONTH));
+               endDatePicker.setTitle("Дата завершения работы");
+               endDatePicker.show();
+           }
+       };
+        binding.dateRange.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(Employee employee) {
-                binding.employee.setText(employeeViewModel.getPickedEmployee().getValue().name);
+            public void onClick(View v) {
+                DatePickerDialog startDatePicker = new DatePickerDialog(
+                        getContext(),startDatePickListener,
+                        actualCalendar.get(Calendar.YEAR),
+                        actualCalendar.get(Calendar.MONTH),
+                        actualCalendar.get(Calendar.DAY_OF_MONTH));
+                startDatePicker.setTitle("Дата начала работы");
+                startDatePicker.show();
             }
-        };
-        employeeViewModel.getPickedEmployee().observe(getViewLifecycleOwner(),pickedEmployeeObserver);
+        });
 
    }
 
+   private void searchEmployeeInit(){
+       binding.searchEmployee.setOnClickListener((v) ->{
+           EmployeeSearchDialog searchDialog = new EmployeeSearchDialog();
+           searchDialog.show(getParentFragmentManager(),"search_fragment");
+       });
+       Observer<Employee> pickedEmployeeObserver = new Observer<Employee>() {
+           @Override
+           public void onChanged(Employee employee) {
+               binding.employee.setText(employeeViewModel.getPickedEmployee().getValue().name);
+           }
+       };
+       employeeViewModel.getPickedEmployee().observe(getViewLifecycleOwner(),pickedEmployeeObserver);
+   }
 
+   private void inputEmployeeMonitoringInit(){
+        binding.employee.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().matches(" *"))
+                    binding.searchEmployee.setColorFilter(getContext()
+                            .getResources()
+                            .getColor(com.beardedhen.androidbootstrap.R.color.bootstrap_gray_lighter));
+                else
+                    binding.searchEmployee
+                            .setColorFilter(getContext()
+                                    .getResources().getColor(com.beardedhen.androidbootstrap.R.color.bootstrap_brand_success));
+            }
+        });
+   }
+   private void sendDataFilter(){
+        binding.sendTaskBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Task task = new Task(
+                        binding.description.getText().toString(),
+                        binding.taskName.getText().toString(),
+                        binding.startDate.getText().toString(),
+                        binding.endDate.getText().toString(),
+                        binding.employee.getText().toString(),
+                        binding.creatorName.getText().toString());
+                taskViewModel.taskIsValid(task);
+            }
+        });
+   }
+
+   private void initFilterStatus(){
+        Observer<String> statusObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                if (s.equals("ok")) Toast.makeText(getContext(), "Задача отправлена", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getContext(), s, Toast.LENGTH_LONG).show();
+            }
+        };
+        taskViewModel.getTaskValidationStatus().observe(getViewLifecycleOwner(),statusObserver);
+   }
 
 }
--- app/src/main/java/com/pavel/databaseapp/taskfragment/CreateTaskViewModel.java
+++ app/src/main/java/com/pavel/databaseapp/taskfragment/CreateTaskViewModel.java
@@ -1,7 +1,34 @@
 package com.pavel.databaseapp.taskfragment;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class CreateTaskViewModel extends ViewModel {
+import com.pavel.databaseapp.data.Employee;
+import com.pavel.databaseapp.data.Task;
 
+public class CreateTaskViewModel extends ViewModel {
+    private MutableLiveData<String> taskValidationStatus;
+    private MutableLiveData<String> datePickStatus;
+    public CreateTaskViewModel(){
+        this.taskValidationStatus = new MutableLiveData<>();
+        this.datePickStatus = new MutableLiveData<>();
+    }
+
+    public void taskIsValid(Task task){
+        if (task.employee == null || task.employee.matches(" *")) taskValidationStatus.setValue("Укажите имя работника или выберите из списка");
+        else if (task.taskName == null || task.taskName.matches(" *")) taskValidationStatus.setValue("Имя задачи пустое");
+        else if (task.description == null || task.description.matches("[A-Za-z]{0,5}| *")) taskValidationStatus.setValue("Описание должно быть больше 5 символов");
+        else if (task.startDate == null || task.endDate == null ||
+                task.startDate.matches(" *") || task.endDate.matches(" *")) taskValidationStatus.setValue("Выбор даты обязателен");
+        else if (task.creator == null) taskValidationStatus.setValue("Неизвестная ошибка, автор неуказан");
+        else taskValidationStatus.setValue("ok");
+    }
+
+    public MutableLiveData<String> getTaskValidationStatus() {
+        return taskValidationStatus;
+    }
+
+    public MutableLiveData<String> getDatePickStatus() {
+        return datePickStatus;
+    }
 }
