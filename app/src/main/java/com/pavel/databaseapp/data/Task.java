3b914cf8ce73152098472f769e8b66aa05c54017
lubuntum <lubuntum@gmail.com>
2022-12-16 13:11:42 +0700

    Подправил формат данных + изменил цвет темы, мелкие исправления

--- app/src/main/java/com/pavel/databaseapp/data/Task.java
+++ app/src/main/java/com/pavel/databaseapp/data/Task.java
@@ -9,6 +9,7 @@ public class Task{
     public static final String END_DATE = "endDate";
     public static final String EMPLOYEE = "employee";
     public static final String CREATOR = "creator";
+    public static final String EMPLOYEE_MAIL = "employeeMail";
 
     public String description;
     public String taskName;
@@ -16,19 +17,21 @@ public class Task{
     public String endDate;
     public String employee;
     public String creator;
+    public String employeeMail;
 
-    public Task(String description, String taskName, String startDate, String endDate, String employee, String creator) {
+    public Task(String description, String taskName, String startDate, String endDate, String employee, String creator, String employeeMail) {
         this.description = description;
         this.taskName = taskName;
         this.startDate = startDate;
         this.endDate = endDate;
         this.employee = employee;
         this.creator = creator;
+        this.employeeMail = employeeMail;
     }
 
     public static Task parse(QueryDocumentSnapshot doc){
         return new Task(doc.getString(DESCRIPTION),doc.getString(TASK_NAME),doc.getString(START_DATE)
-                ,doc.getString(END_DATE),doc.getString(EMPLOYEE),doc.getString(CREATOR));
+                ,doc.getString(END_DATE),doc.getString(EMPLOYEE),doc.getString(CREATOR),doc.getString(EMPLOYEE_MAIL));
     }
     public String getDateRange(){
         return String.format("%s => %s",startDate,endDate);
