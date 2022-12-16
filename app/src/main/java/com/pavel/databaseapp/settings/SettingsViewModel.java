7683d4e304c478ace28236fab5dec26f3d35965d
lubuntum <lubuntum@gmail.com>
2022-12-16 13:12:04 +0700

    Добавил важные ресурсы

--- app/src/main/java/com/pavel/databaseapp/settings/SettingsViewModel.java
+++ app/src/main/java/com/pavel/databaseapp/settings/SettingsViewModel.java
@@ -1,9 +1,17 @@
 package com.pavel.databaseapp.settings;
 
 public class SettingsViewModel {
+    //Network
     public static final String EMPLOYEE_COLLECTION = "employees";
     public static final String TASKS_COLLECTION = "tasks";
 
-    public static final String EMPLOYEE_NAME = "Павел";
-    public static final String CREATOR_NAME = "Елена Павловна";
+    public static final String EMPLOYEE_NAME = "Павел";//test
+    public static final String CREATOR_NAME = "Елена Павловна";//test
+
+    /*APP Local Data*/
+    public static final String SETTINGS_STORAGE = "settings";
+    public static final String LOGIN_KEY = "login";
+    public static final String PASSWORD_KEY = "password";
+    public static final String CURRENT_THEME = "theme";
+    public static final String EMPLOYEE = "employee";
 }
