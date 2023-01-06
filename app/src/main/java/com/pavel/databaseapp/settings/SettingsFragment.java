1d5ad79cf823d9f6df955d05c44310aca6608216
lubuntum <lubuntum@gmail.com>
2023-01-06 20:41:36 +0700

    Реализовал загрузку всех актуальных данных об аккаунте в настройках, реализовал фрагмент настроект

--- app/src/main/java/com/pavel/databaseapp/settings/SettingsFragment.java
+++ app/src/main/java/com/pavel/databaseapp/settings/SettingsFragment.java
@@ -1,4 +1,46 @@
 package com.pavel.databaseapp.settings;
 
-public class SettingsFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.pavel.databaseapp.R;
+import com.pavel.databaseapp.databinding.FragmentSettingsBinding;
+import com.pavel.databaseapp.mytask.MyTaskViewModel;
+
+public class SettingsFragment extends Fragment {
+    SettingsViewModel settingsViewModel;
+    MyTaskViewModel myTaskViewModel;
+    FragmentSettingsBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
+        myTaskViewModel = new ViewModelProvider(getActivity()).get(MyTaskViewModel.class);
+        binding = FragmentSettingsBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.userName.setText(
+                String.format(getResources().getString(R.string.user_name),
+                        settingsViewModel.getEmployee().getSecondName(),
+                        settingsViewModel.getEmployee().getName())
+        );
+        binding.position.setText(
+                String.format(getResources().getString(R.string.current_position),
+                        settingsViewModel.getEmployee().getPosition()));
+        if(myTaskViewModel.getMutableTasks() != null &&
+                myTaskViewModel.getMutableTasks().getValue() != null)
+            binding.activeTasks.setText(String.format(getResources().getString(R.string.current_task_count),
+                    myTaskViewModel.getMutableTasks().getValue().size()));
+    }
 }
--- app/src/main/java/com/pavel/databaseapp/settings/SettingsViewModel.java
+++ app/src/main/java/com/pavel/databaseapp/settings/SettingsViewModel.java
@@ -1,17 +1,45 @@
 package com.pavel.databaseapp.settings;
 
-public class SettingsViewModel {
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.google.gson.Gson;
+import com.pavel.databaseapp.data.Employee;
+
+public class SettingsViewModel extends AndroidViewModel {
     //Network
     public static final String EMPLOYEE_COLLECTION = "employees";
     public static final String TASKS_COLLECTION = "tasks";
 
-    public static final String EMPLOYEE_NAME = "Павел";//test
-    public static final String CREATOR_NAME = "Елена Павловна";//test
-
     /*APP Local Data*/
     public static final String SETTINGS_STORAGE = "settings";
-    public static final String LOGIN_KEY = "login";
-    public static final String PASSWORD_KEY = "password";
+    public static final String LOGIN_KEY = "login";//deprecate
+    public static final String PASSWORD_KEY = "password";//deprecate
     public static final String CURRENT_THEME = "theme";
     public static final String EMPLOYEE = "employee";
+
+    private SharedPreferences preferences;
+    private Employee employee;
+    public SettingsViewModel(@NonNull Application application) {
+        super(application);
+        this.preferences = application.getSharedPreferences(SETTINGS_STORAGE, Context.MODE_PRIVATE);
+        employeeInit();
+    }
+    public void employeeInit(){
+        Gson gson = new Gson();
+        this.employee =
+                gson.fromJson(preferences.getString(EMPLOYEE,null),Employee.class);
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public SharedPreferences getPreferences() {
+        return preferences;
+    }
 }
